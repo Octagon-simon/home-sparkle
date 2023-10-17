76d53619217aeb986be0a209e9ec0f37fd896968
Octagon <ugorji757@gmail.com>
2023-10-17 15:42:40 +0100

    completed contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,102 +1,47 @@
+"use client"
+
 import '@/styles/root.css'
 import styles from './page.module.css';
 import Image from 'next/image';
 
-export default function About() {
+export default function Contact() {
 
     return (
         <main className={styles.main}>
-            <section className={`section ${styles.aboutTopContainer}`}>
-                <div className="container">
-                    <div className={`section-content ${styles.mx500}`}>
-                        <h4 className={`title is-4 ${styles.aboutTopText}`}>We are a company that prioritize customer&apos;s satisfaction</h4>
+            <section>
+                <div className='columns'>
+                    <div className={`column is-6 ${styles.columnWithImage}`}>
+                        <Image src={'./contact-hero.svg'} width={500} height={500} />
                     </div>
-                </div>
-            </section>
-            <section className={`hero is-medium ${styles.heroBg}`}>
-            </section>
-            <section className={styles.missionWrapper}>
-                <div className={styles.mission}>
-                    <div className={styles.missionIcon}>
-                        <Image src="/mission-goal.jpg" width={25} height={25} /> <span>Mission</span>
-                    </div>
-                    <div className={styles.missionText}>
-                        <p>Lörem ipsum trassa plogga möling. Monoprebelt fasatt men skimma. Mäskap tritise i onade utom biogisk, att rit-avdrag. Eposade farir, anteng även om neresön holl. Tinat makrokirat såsom multinar. </p>
-                    </div>
-                </div>
-            </section>
-            <section className='section'>
-                <div className='container'>
-                    <div className={`section-content ${styles.mx500} m-auto has-text-centered`}>
-                        <h4 className={`title is-3 ${styles.fw500} ${styles.missionLargeText}`}>We turn mess into marvelous.
-                            <br /> Discover the power of a spotless space</h4>
-                    </div>
-                    <div className={`${styles.projectsCompleted}`}>
-                        <div className={styles.projectsCompletedItem}>
-                            <p>4000+</p>
-                            <p>Projects Completed</p>
-                        </div>
-                        <div className={styles.projectsCompletedItem}>
-                            <p>25</p>
-                            <p>Cities covered wiithin the UK</p>
-                        </div>
-                        <div className={styles.projectsCompletedItem}>
-                            <p>12</p>
-                            <p>Awards earned</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className={`section ${styles.offBg}`}>
-                <div className='container'>
-                    <div className={styles.sparkleContainer}>
-                        <p className={styles.sparkleHeading}>HomeSparkle Solution</p>
-                        <p className={styles.sparkleContent}>Lörem ipsum trassa plogga möling. Monoprebelt fasatt men skimma. Mäskap tritise i onade utom biogisk, att rit-avdrag. Eposade farir, anteng även om neresön holl. Tinat makrokirat såsom multinar. Lörem ipsum trassa plogga möling. Monoprebelt fasatt men skimma. Mäskap tritise i onade utom biogisk, att rit-avdrag. Eposade farir, anteng även om neresön holl. Tinat makrokirat såsom multinar. Lörem ipsum trassa plogga möling. Monoprebelt fasatt men skimma. Mäskap tritise i onade utom biogisk, att rit-avdrag. Eposade farir, anteng även om neresön holl. Tinat makrokirat såsom multinar. </p>
-                    </div>
-                </div>
-            </section>
-            <section className='section'>
-                <div className='container'>
-                    <div className={`section-content has-text-centered m-auto ${styles.mx700}`}>
-                        <h4 className='title is-3'>Our Team</h4>
-                        <p className={`${styles.fw400}`}>Meet the faces that make our mission possible and learn more about the talent and commitment that sets us apart.</p>
-                    </div>
-                    <div className={styles.teamWrapper}>
-                        <div className={styles.teamItem}>
-                            <div className={styles.teamImage}>
-                                <Image src={'./team-1.jpg'} width={150} height={150} />
+                    <div className='column is-6 is-align-self-center'>
+                        <div className={styles.formWrapper}>
+                            <div className={`section-content`}>
+                                <h4 className={`title is-4 ${styles.contactText}`}>We want to hear from you!</h4>
+                                <p>Our customer support is always active 24/7e</p>
                             </div>
-                            <div className={styles.teamMeta}>
-                                <p>Macdonald A.</p>
-                                <p>CEO, HomeSparkle Solution</p>
-                            </div>
-                        </div>
-                        <div className={styles.teamItem}>
-                            <div className={styles.teamImage}>
-                                <Image src={'./team-2.jpg'} width={150} height={150} />
-                            </div>
-                            <div className={styles.teamMeta}>
-                                <p>Macdonald A.</p>
-                                <p>CEO, HomeSparkle Solution</p>
-                            </div>
-                        </div>
-                        <div className={styles.teamItem}>
-                            <div className={styles.teamImage}>
-                                <Image src={'./team-3.jpg'} width={150} height={150} />
-                            </div>
-                            <div className={styles.teamMeta}>
-                                <p>Macdonald A.</p>
-                                <p>CEO, HomeSparkle Solution</p>
-                            </div>
-                        </div>
-                        <div className={styles.teamItem}>
-                            <div className={styles.teamImage}>
-                                <Image src={'./team-4.jpg'} width={150} height={150} />
-                            </div>
-                            <div className={styles.teamMeta}>
-                                <p>Macdonald A.</p>
-                                <p>CEO, HomeSparkle Solution</p>
-                            </div>
+                            <form className='mt-4' method='post' onSubmit={(e) => {
+                                e.preventDefault()
+                            }}>
+                                <div className='field'>
+                                    <label className='label'>Full name</label>
+                                    <input className={`input ${styles.input}`} />
+                                </div>
+                                <div className='field'>
+                                    <label className='label'>Phone number</label>
+                                    <input className={`input ${styles.input}`} />
+                                </div>
+                                <div className='field'>
+                                    <label className='label'>Location</label>
+                                    <input className={`input ${styles.input}`} />
+                                </div>
+                                <div className='field'>
+                                    <label className='label'>Your message</label>
+                                    <textarea className={`input ${styles.input}`}></textarea>
+                                </div>
+                                <div className='field mt-5'>
+                                    <button className='button btn-brand'>Send message</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
