e02a0d59c20c503550d54a8fdd8b2260d14b713e
Octagon <ugorji757@gmail.com>
2023-10-17 14:46:24 +0100

    completed services page

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -6,32 +6,57 @@ export default function Services() {
 
     return (
         <main className={styles.main}>
-            <section className={`section ${styles.aboutTopContainer}`}>
-                <div className="container">
-                    <div className={`section-content ${styles.mx500}`}>
-                        <h4 className={`title is-4 ${styles.aboutTopText}`}>We are a company that prioritize customer&apos;s satisfaction</h4>
-                    </div>
-                </div>
-            </section>
-            <section className={`hero is-medium ${styles.heroBg}`}>
-            </section>
-            <section className={styles.missionWrapper}>
-                <div className={styles.mission}>
-                    <div className={styles.missionIcon}>
-                        <Image src="/mission-goal.jpg" width={25} height={25} /> <span>Mission</span>
-                    </div>
-                    <div className={styles.missionText}>
-                        <p>Lörem ipsum trassa plogga möling. Monoprebelt fasatt men skimma. Mäskap tritise i onade utom biogisk, att rit-avdrag. Eposade farir, anteng även om neresön holl. Tinat makrokirat såsom multinar. </p>
-                    </div>
-                </div>
-            </section>
             <section className='section'>
+                <div className='container'>
+                    <div className={`section-content m-auto has-text-centered ${styles.mx500}`}>
+                        <h4 className={`title is-3 ${styles.serviceText}`}>Some of our services</h4>
+                        <p>A selection of the services we provide</p>
+                    </div>
+                </div>
+            </section>
+            {/* <section className={`hero is-medium ${styles.heroBg}`}>
+            </section> */}
+            <section className={styles.servicesHeroWrapper}>
+                <div className={styles.servicesHeroItem}>
+                    <div className={styles.servicesHeroItemTextBox}>
+                        <p>House and office Cleaning</p>
+                    </div>
+                    <div className={`${styles.servicesHeroItemImageBox}`}>
+                        <Image src="./services-img-1.jpg" width={200} height={200} alt='' />
+                    </div>
+                </div>
+                <div className={styles.servicesHeroItem}>
+                    <div className={styles.servicesHeroItemTextBox}>
+                        <p>Move-In/Move-Out Cleaning</p>
+                    </div>
+                    <div className={`${styles.servicesHeroItemImageBox}`}>
+                        <Image src="./services-img-2.jpg" width={200} height={200} alt='' />
+                    </div>
+                </div>
+                <div className={styles.servicesHeroItem}>
+                    <div className={`${styles.servicesHeroItemImageBox}`}>
+                        <Image src="./services-img-3.jpg" width={200} height={200} alt='' />
+                    </div>
+                    <div className={styles.servicesHeroItemTextBox}>
+                        <p>House and office Cleaning</p>
+                    </div>
+                </div>
+                <div className={styles.servicesHeroItem}>
+                    <div className={`${styles.servicesHeroItemImageBox}`}>
+                        <Image src="./services-img-4.jpg" width={200} height={200} alt='' />
+                    </div>
+                    <div className={styles.servicesHeroItemTextBox}>
+                        <p>Window Cleaning and others</p>
+                    </div>
+                </div>
+            </section>
+            <section className={`section ${styles.sectionWithPadding}`}>
                 <div className='container'>
                     <div className={`section-content ${styles.mx500} m-auto has-text-centered`}>
                         <h4 className={`title is-3 ${styles.fw500} ${styles.missionLargeText}`}>We turn mess into marvelous.
                             <br /> Discover the power of a spotless space</h4>
                     </div>
-                    <div className={`${styles.projectsCompleted}`}>
+                    <div className={`${styles.projectsCompleted} mt-5`}>
                         <div className={styles.projectsCompletedItem}>
                             <p>4000+</p>
                             <p>Projects Completed</p>
@@ -43,60 +68,6 @@ export default function Services() {
                         <div className={styles.projectsCompletedItem}>
                             <p>12</p>
                             <p>Awards earned</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className={`section ${styles.offBg}`}>
-                <div className='container'>
-                    <div className={styles.sparkleContainer}>
-                        <p className={styles.sparkleHeading}>HomeSparkle Solution</p>
-                        <p className={styles.sparkleContent}>Lörem ipsum trassa plogga möling. Monoprebelt fasatt men skimma. Mäskap tritise i onade utom biogisk, att rit-avdrag. Eposade farir, anteng även om neresön holl. Tinat makrokirat såsom multinar. Lörem ipsum trassa plogga möling. Monoprebelt fasatt men skimma. Mäskap tritise i onade utom biogisk, att rit-avdrag. Eposade farir, anteng även om neresön holl. Tinat makrokirat såsom multinar. Lörem ipsum trassa plogga möling. Monoprebelt fasatt men skimma. Mäskap tritise i onade utom biogisk, att rit-avdrag. Eposade farir, anteng även om neresön holl. Tinat makrokirat såsom multinar. </p>
-                    </div>
-                </div>
-            </section>
-            <section className='section'>
-                <div className='container'>
-                    <div className={`section-content has-text-centered m-auto ${styles.mx700}`}>
-                        <h4 className='title is-3'>Our Team</h4>
-                        <p className={`${styles.fw400}`}>Meet the faces that make our mission possible and learn more about the talent and commitment that sets us apart.</p>
-                    </div>
-                    <div className={styles.teamWrapper}>
-                        <div className={styles.teamItem}>
-                            <div className={styles.teamImage}>
-                                <Image src={'./team-1.jpg'} width={150} height={150} />
-                            </div>
-                            <div className={styles.teamMeta}>
-                                <p>Macdonald A.</p>
-                                <p>CEO, HomeSparkle Solution</p>
-                            </div>
-                        </div>
-                        <div className={styles.teamItem}>
-                            <div className={styles.teamImage}>
-                                <Image src={'./team-2.jpg'} width={150} height={150} />
-                            </div>
-                            <div className={styles.teamMeta}>
-                                <p>Macdonald A.</p>
-                                <p>CEO, HomeSparkle Solution</p>
-                            </div>
-                        </div>
-                        <div className={styles.teamItem}>
-                            <div className={styles.teamImage}>
-                                <Image src={'./team-3.jpg'} width={150} height={150} />
-                            </div>
-                            <div className={styles.teamMeta}>
-                                <p>Macdonald A.</p>
-                                <p>CEO, HomeSparkle Solution</p>
-                            </div>
-                        </div>
-                        <div className={styles.teamItem}>
-                            <div className={styles.teamImage}>
-                                <Image src={'./team-4.jpg'} width={150} height={150} />
-                            </div>
-                            <div className={styles.teamMeta}>
-                                <p>Macdonald A.</p>
-                                <p>CEO, HomeSparkle Solution</p>
-                            </div>
                         </div>
                     </div>
                 </div>
